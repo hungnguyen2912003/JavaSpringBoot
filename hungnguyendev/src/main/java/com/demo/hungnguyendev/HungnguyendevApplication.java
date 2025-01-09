9a59ad1c497ddac5daac911ee0656050357aebce
hungnguyen2912003 <hungthinh291@gmail.com>
2025-01-09 21:42:45 +0700

    Initial Project

---
+++ hungnguyendev/src/main/java/com/demo/hungnguyendev/HungnguyendevApplication.java
@@ -0,0 +1,13 @@
+package com.demo.hungnguyendev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HungnguyendevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HungnguyendevApplication.class, args);
+	}
+
+}
---
+++ hungnguyendev/src/test/java/com/demo/hungnguyendev/HungnguyendevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.hungnguyendev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HungnguyendevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
